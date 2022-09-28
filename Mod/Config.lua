036d6a71d35d74778aff56c8cf8ff62477e27998
Donald Nie <reachingfoul@gmail.com>
2022-09-27 20:55:13 -0500

    Move Dir

---
+++ Mod/Config.lua
@@ -0,0 +1,12 @@
+﻿return {
+	Title = "自动恢复入魔者",
+	HasArchive = false,
+	BackendPlugins = {
+		[1] = "RestoreInfected.dll",
+	},
+	Author = "ReachingFoul",
+	Description = "移动到各自格子后，恢复入魔者",
+    DefaultSettings = {
+
+	}
+}
