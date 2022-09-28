24ebc55343f9e985c24b26d2d37950ccc9ed4b0d
IcedLife <reachingfoul@gmail.com>
2022-09-27 20:52:43 -0500

    Initial Commit

---
+++ DebugLog.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddInfected
+{
+    internal class DebugLog
+    {
+        static public void debug(String message)
+        {
+            FileLog.Log(DateTime.Now.ToString("o")+ " | " + message);
+        }
+
+        public static void checkNull(Object obj, String varName)
+        {
+           String ret = obj == null ? "Null" : "Not Null";
+            debug(varName + " is " + ret); 
+        }
+    }
+
+
+}
